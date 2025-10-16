d1867df95d10a7b8d807fbb5f4da3329bd54710c
Tranchinh2901 <chinh99078@donga.edu.vn>
2025-10-16 22:28:21 +0700

    edit review schema

--- src/modules/reviews/schema/review.schema.ts
+++ src/modules/reviews/schema/review.schema.ts
@@ -21,16 +21,7 @@ export const CreateReviewSchema = Joi.object({
       'string.max': 'Comment must be at most 1000 characters'
     }),
   
-  product_id: Joi.number()
-    .integer()
-    .positive()
-    .required()
-    .messages({
-      'number.base': 'Product ID must be a number',
-      'number.integer': 'Product ID must be an integer',
-      'number.positive': 'Product ID must be positive',
-      'any.required': 'Product ID is required'
-    })
+
 });
 
 export const UpdateReviewSchema = Joi.object({
